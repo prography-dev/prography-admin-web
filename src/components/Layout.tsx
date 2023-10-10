80456680f6105f7dbbb7f53f5752289b22472087
kang <kangju2000@naver.com>
2023-10-11 01:47:58 +0900

    :recycle: sidebar의 href로 값 찾도록 변경

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,21 +1,22 @@
-import { Box, Flex } from "@chakra-ui/react";
-import Header from "./Header";
-import Sidebar from "./Sidebar";
-import type { PropsWithChildren } from "react";
-import { usePathname } from "next/navigation";
-import { sidebarList } from "@/constants/sidebar";
+import { Box, Flex } from '@chakra-ui/react'
+import Header from './Header'
+import Sidebar from './Sidebar'
+import type { PropsWithChildren } from 'react'
+import { usePathname } from 'next/navigation'
+import { sidebarList } from '@/constants/sidebar'
 
 const Layout = ({ children }: PropsWithChildren) => {
-  const pathname = usePathname();
-  const currentPath = `/${pathname.split("/")[1]}`;
+  const pathname = usePathname()
 
-  const sidebarProps = sidebarList.find(({ items }) =>
-    items[0].href.startsWith(currentPath),
-  );
+  const sidebarProps = sidebarList.find((sidebar) => pathname.startsWith(sidebar.href))
+
+  if (!sidebarProps) {
+    return children
+  }
 
   return (
     <>
-      <Header currentPath={currentPath} />
+      <Header currentPath={sidebarProps.href} />
       <Flex pt="80px" h="100vh">
         {sidebarProps && <Sidebar {...sidebarProps} />}
         <Box as="main" flex="1" p="36px">
@@ -23,7 +24,7 @@ const Layout = ({ children }: PropsWithChildren) => {
         </Box>
       </Flex>
     </>
-  );
-};
+  )
+}
 
-export default Layout;
+export default Layout
--- src/constants/sidebar.ts
+++ src/constants/sidebar.ts
@@ -1,111 +1,112 @@
-import Sidebar from "@/components/Sidebar";
-import type { ComponentProps } from "react";
+import Sidebar from '@/components/Sidebar'
+import type { ComponentProps } from 'react'
 
-const authSidebar: ComponentProps<typeof Sidebar> = {
-  title: "계정 관리",
+type SidebarProps = ComponentProps<typeof Sidebar> & { href: string }
+
+const authSidebar: SidebarProps = {
+  title: '계정 관리',
+  href: '/auth',
   items: [
     {
-      label: "로그인",
-      href: "/auth/login",
+      label: '로그인',
+      href: '/auth/login',
     },
     {
-      label: "운영진 계정 등록",
-      href: "/auth/signup",
+      label: '운영진 계정 등록',
+      href: '/auth/signup',
     },
     {
-      label: "계정 상세 정보 편집",
-      href: "/auth/edit",
+      label: '계정 상세 정보 편집',
+      href: '/auth/edit',
     },
     {
-      label: "권한 관리",
-      href: "/auth/permission",
+      label: '권한 관리',
+      href: '/auth/permission',
     },
   ],
-};
+}
 
-const memberSidebar: ComponentProps<typeof Sidebar> = {
-  title: "회원 관리",
+const memberSidebar: SidebarProps = {
+  title: '회원 관리',
+  href: '/member',
   items: [
     {
-      label: "현 기수 관리",
-      href: "/member",
+      label: '현 기수 관리',
+      href: '/member',
       subItems: [
         {
-          label: "대시보드",
-          href: "/member/dashboard",
+          label: '대시보드',
+          href: '/member/dashboard',
         },
         {
-          label: "출석체크",
-          href: "/member/attendance",
+          label: '출석체크',
+          href: '/member/attendance',
         },
       ],
     },
     {
-      label: "이전 기수 데이터",
-      href: "/member/previous",
+      label: '이전 기수 데이터',
+      href: '/member/previous',
     },
   ],
-};
+}
 
-const sessionSidebar: ComponentProps<typeof Sidebar> = {
-  title: "세션/마일스톤 관리",
+const sessionSidebar: SidebarProps = {
+  title: '세션/마일스톤 관리',
+  href: '/session',
   items: [
     {
-      label: "새 기수 시작",
-      href: "/session/new",
+      label: '새 기수 시작',
+      href: '/session/new',
     },
     {
-      label: "현 기수 관리",
-      href: "/session",
+      label: '현 기수 관리',
+      href: '/session',
       subItems: [
         {
-          label: "마일스톤 & 세션",
-          href: "/session",
+          label: '마일스톤 & 세션',
+          href: '/session',
         },
       ],
     },
     {
-      label: "이전 기수 데이터",
-      href: "/session/previous",
+      label: '이전 기수 데이터',
+      href: '/session/previous',
     },
   ],
-};
+}
 
-const recruitSidebar: ComponentProps<typeof Sidebar> = {
-  title: "리쿠르팅",
+const recruitSidebar: SidebarProps = {
+  title: '리쿠르팅',
+  href: '/recruit',
   items: [
     {
-      label: "새 기수 시작",
-      href: "/recruit",
+      label: '새 기수 시작',
+      href: '/recruit',
     },
     {
-      label: "홍보 관리",
-      href: "/recruit/promotion/channel",
+      label: '홍보 관리',
+      href: '/recruit/promotion/channel',
       subItems: [
         {
-          label: "홍보 채널 관리",
-          href: "/recruit/promotion/channel",
+          label: '홍보 채널 관리',
+          href: '/recruit/promotion/channel',
         },
         {
-          label: "홍보 문구 관리",
-          href: "/recruit/promotion/text",
+          label: '홍보 문구 관리',
+          href: '/recruit/promotion/text',
         },
       ],
     },
     {
-      label: "대시보드",
-      href: "/recruit/dashboard",
+      label: '대시보드',
+      href: '/recruit/dashboard',
     },
     {
-      label: "선발하기",
-      href: "/recruit/selection",
+      label: '선발하기',
+      href: '/recruit/selection',
     },
   ],
-};
+}
 
-export const sidebarList = [
-  authSidebar,
-  memberSidebar,
-  sessionSidebar,
-  recruitSidebar,
-];
+export const sidebarList = [authSidebar, memberSidebar, sessionSidebar, recruitSidebar]
