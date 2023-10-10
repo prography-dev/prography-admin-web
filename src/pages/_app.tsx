16f06de2b1296e7082589450a745c31874ae3b49
kang <kangju2000@naver.com>
2023-10-10 19:24:46 +0900

    :lipstick: global theme 설정

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,23 @@
 import Header from "@/components/Header";
-import { Box, ChakraProvider } from "@chakra-ui/react";
+import { Box, ChakraProvider, extendTheme } from "@chakra-ui/react";
 import { AppProps } from "next/app";
+
+const theme = extendTheme({
+  styles: {
+    global: {
+      "html, body": {
+        width: "100%",
+        height: "100%",
+      },
+    },
+  },
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <Header />
-      <Box as="main" pt="80px">
+      <Box as="main" pt="80px" h="100vh">
         <Component {...pageProps} />
       </Box>
     </ChakraProvider>
