b8089d6483b95f88bd9ab95d5bbc1ff1fb3bafb4
kang <kangju2000@naver.com>
2023-10-10 23:06:12 +0900

    :wrench: next redirect 설정

--- next.config.js
+++ next.config.js
@@ -1,6 +1,22 @@
+
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  redirects: async () => {
+    return [
+      {
+        source: '/',
+        destination: '/auth/login', // 추후 middleware로 로그인 여부 확인 후 분기처리
+        permanent: true
+      },
+      {
+        source: '/member',
+        destination: `/member/dashboard`,
+        permanent: true
+      },
+    ]
+  }
 }
 
 module.exports = nextConfig
